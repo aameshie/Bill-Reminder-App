f115fe944ede800bccfe50f75775fab4f873cd98
elameshie <elameshie@mobilus.com>
2024-03-30 20:54:39 -0400

    Will prompt user to enter information

--- src/BillReminderApp.java
+++ src/BillReminderApp.java
@@ -1,14 +1,32 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class BillReminderApp {
-    public static void main(String[] args) {
-        BillManager manager = new BillManager();
+  public static void main(String[] args) {
+    BillManager manager = new BillManager();
+    Scanner scanner = new Scanner(System.in);
+    DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        //Example bills
-        manager.addBill(new Bill("Internet", LocalDate.of(2024, 4, 15), LocalDate.of(2024, 4, 10)));
-        manager.addBill(new Bill("Electricity", LocalDate.of(2024, 4, 20), LocalDate.of(2024, 4, 15)));
+    System.out.println("Bill Reminder App");
+    System.out.println("Enter the number of bills you want to add: ");
 
-        // Check for reminders
-        manager.checkReminders();
+    int numBills = scanner.nextInt();
+    scanner.nextLine();
 
+    for (int i = 0; i < numBills; i++) {
+      System.out.println("Enter bill # " + (i + 1) + " information:");
+
+      System.out.print("Name: ");
+      String name = scanner.nextLine();
     }
+
+    // Example bills
+    manager.addBill(new Bill("Internet", LocalDate.of(2024, 4, 15), LocalDate.of(2024, 4, 10)));
+    manager.addBill(new Bill("Electricity", LocalDate.of(2024, 4, 20), LocalDate.of(2024, 4, 15)));
+
+    // Check for reminders
+    manager.checkReminders();
+
+  }
 }
